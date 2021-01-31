3e35288144180b4ef739f78c9e61a00a35b5dd81
Maralov Kemal <37023297+pikopiko15@users.noreply.github.com>
2021-01-31 18:07:48 +0600

    Add project files.

---
+++ WebDevLabs/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebDevLabs.Models;
+
+namespace WebDevLabs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Inform()
+        {
+            return View();
+        }
+    }
+}
